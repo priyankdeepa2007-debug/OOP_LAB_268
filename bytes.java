43ca2a2915cf762f37b030eb84a25845f5b8b0fe
priyankdeepa2007-debug <priyankdeepa2007@gmail.com>
2025-12-29 12:36:20 +0530

    Add files via upload

---
+++ bytes.java
@@ -0,0 +1,24 @@
+public class bytes{
+       public static void main(String agrs[]){
+       byte a=126;
+       byte b=100;
+       long c=1000;
+       long d=12;
+       short e=7;
+       short f=1; 
+       int h =56;
+       float ang=1.22f;
+       double p=1.2222268905;
+       char g = 'j';
+       
+       System.out.println(a+b);
+       System.out.println(c*d);
+       System.out.println(e/f);
+       System.out.println(ang++);
+       System.out.println(h);
+       System.out.println(p);
+       System.out.println(g);
+}
+}
+       
+       
