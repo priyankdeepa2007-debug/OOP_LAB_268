692ee99165d35601b5d7bd52f70c309f4610f9c8
priyankdeepa2007-debug <priyankdeepa2007@gmail.com>
2025-12-29 12:39:41 +0530

    Add files via upload

---
+++ file.java
@@ -0,0 +1,16 @@
+class file{
+      public static void main(String agrs[]){
+      int a=30;
+      int b=60;
+      int c=90;
+      int d=115;
+      boolean e=true;
+      System.out.println((a+b)-c/9);
+      System.out.println(a==b);
+      System.out.println((d-c/11)*(b/a));
+      System.out.println((a>b)&&(c<d));
+      System.out.println((a<c)||(b>d));
+      System.out.println(!e);
+}
+}
+      
