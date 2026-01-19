3a11dc2d51778e0eb90192a2b0422a852ed0eded
priyankdeepa2007-debug <priyankdeepa2007@gmail.com>
2026-01-19 12:13:32 +0530

    Add files via upload

---
+++ retail.java
@@ -0,0 +1,15 @@
+
+public class retail{
+       public static void main(String[]args){
+       boolean citizen = true;
+       int age=18;
+       if(true){
+       System.out.println("the citizen of the country");
+       if(age>18)
+       System.out.println("eligible to vote");
+       else
+      System.out.println(" not eligible to vote");
+}
+       
+}
+}
