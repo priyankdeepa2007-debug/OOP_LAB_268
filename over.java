a3112504e7d54bfa04b65ae83fec0ac83d8f2a28
priyankdeepa2007-debug <priyankdeepa2007@gmail.com>
2026-03-16 12:08:56 +0530

    Add files via upload

---
+++ over.java
@@ -0,0 +1,17 @@
+	    public class over{
+		public static void main(String[]args){
+			cal A = new cal();
+			System.out.println(A.method(13,12));
+	}
+	}
+	public class cal {
+	int method(int a, int b, int c){
+		 return ((a+b)/c);
+	}
+	int method(int a,int b){
+		return (a+b);
+	}
+	int method (int a,int b,int c, int d){
+		return((a*b*c)+d);
+}
+}
